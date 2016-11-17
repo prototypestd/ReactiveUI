0e142669590c4cedaa545014d57649ec39d1dd01
Haikal Izzuddin <heywhite69@gmail.com>
2016-11-17 22:29:59 +0800

    html: js: Laying out the layouts for post-render navigation building

--- html-resources/js/buildNav.js
+++ html-resources/js/buildNav.js
@@ -1,11 +1,18 @@
 ﻿$(document).ready(function () {
     var nav = document.getElementById('navigation');
     var text;
+    var navItems = ["test", "test2", "test3"];
     
-    alert(navigationCommands.navItems);
-    for (i = 0; i < navigationCommands.navItems.length; i++) {
-        text += "<li>" + navigationCommands.navItems[i] + "</li>";
+    for (i = 0; i < navItems.length; i++) {
+
+        text += "<li>";
+        text += "<a href='" + navItems[i] + ".html' class='active'>";
+        text += "<i class='fa fa-dashboard icon'> <b class='bg-danger'></b> </i>";
+        text += "<span>"+ navItems[i] +"</span>";
+        text += "</a>";
+        text += "</li>";
+
     }
 
-    nav.innerHTML = text;
+    nav.innerHTML += text;
 });

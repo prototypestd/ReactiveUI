c5abc938e927b3075ade6a3bc901bce176dfefd6
Haikal Izzuddin <heywhite69@gmail.com>
2016-11-17 13:29:57 +0800

    Constants: derp

--- scripts/Constants.cs
+++ scripts/Constants.cs
@@ -8,5 +8,7 @@ namespace ReactiveUI
         public static string htmlResource = string.Format(@"{0}\html-resources\html\", App.StartupPath);
         public static App app = new App();
         public static Debug debug = new Debug();
+
+        
     }
 }

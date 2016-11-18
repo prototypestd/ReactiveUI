91a2510e8cef014f627077097f0504c8f3011218
Haikal Izzuddin <heywhite69@gmail.com>
2016-11-18 12:51:22 +0800

    Renderer: Allow for navigation items to be added dynamically

--- html-resources/js/buildNav.js
+++ html-resources/js/buildNav.js
@@ -1,14 +1,13 @@
 ﻿$(document).ready(function () {
     var nav = document.getElementById('navigation');
     var text;
-    var navItems = ["test", "test2", "test3"];
-    
-    for (i = 0; i < navItems.length; i++) {
+
+    for (i = 0; i < navigationCommands.navItems.length; i++) {
 
         text += "<li>";
-        text += "<a href='" + navItems[i] + ".html' class='active'>";
+        text += "<a href='" + navigationCommands.navItems[i] + ".html' class='active'>";
         text += "<i class='fa fa-dashboard icon'> <b class='bg-danger'></b> </i>";
-        text += "<span>"+ navItems[i] +"</span>";
+        text += "<span>" + navigationCommands.navItems[i] + "</span>";
         text += "</a>";
         text += "</li>";
 
--- scripts/Commands/NavigationCommands.cs
+++ scripts/Commands/NavigationCommands.cs
@@ -12,7 +12,16 @@ namespace ReactiveUI.Commands
     {
         private static ChromiumWebBrowser _instanceBrowser = null;
         private static Form1 _instanceMainWindow = null;
-        public List<string> navItems = new List<string> { "test", "test2" };
+
+        /// <summary>
+        /// Navigation Items
+        /// </summary>
+        private static List<string> navItem = new List<string>() { "test" };
+        public string[] navItems
+        {
+            get { return navItem.ToArray(); }
+        }
+
 
         public NavigationCommands(ChromiumWebBrowser originalBrowser, Form1 form)
         {
@@ -28,6 +37,18 @@ namespace ReactiveUI.Commands
         public void goBack()
         {
             CefInstance.GoBack(_instanceBrowser);
+        }
+
+        /// <summary>
+        /// Dynamically adds a navigation item to the navList
+        /// </summary>
+        /// <param name="page"></param>
+        public static void AddToNav(string page)
+        {
+            if (!String.IsNullOrEmpty(page))
+            {
+                navItem.Add(page);
+            }
         }
     }
 }

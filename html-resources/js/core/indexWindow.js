d55c31514d46f1b90b149f4520204389abbeb2ce
Haikal Izzuddin <heywhite69@gmail.com>
2017-02-01 23:28:16 +0800

    HTML: Put the JS in a separate file for easier management and readability

---
+++ html-resources/js/core/indexWindow.js
@@ -0,0 +1,48 @@
+﻿new Vue({
+    el: '#reactApp',
+    data: {
+        username: 'haikalizz',
+        curTime: '',
+        notifications: [],
+        curPage: '',
+        notiCount: notificationManager.notiCount,
+    },
+    mounted: function () {
+        this.$nextTick(function () {
+            setInterval(function () {
+                // Gets the date from backend and renders it
+                this.curTime = commonCommands.currentDate;
+
+                // Handles the inline navigation
+                if (this.curPage == "" || this.curPage != navigationCommands.nextPage) {
+                    document.getElementById('ctent').src = navigationCommands.nextPage;
+                    this.curPage = navigationCommands.nextPage;
+                }
+            }.bind(this), 1000)
+        })
+    },
+    methods: {
+        exitApp() {
+            this.$confirm('Are you sure you want to exit?', 'Warning', {
+                confirmButtonText: 'Yes',
+                cancelButtonText: 'No',
+                type: 'warning'
+            }).then(() => {
+                commonCommands.exitApp();
+            }).catch(() => {
+                this.$message({
+                    type: 'info',
+                    message: 'Shutdown Cancelled'
+                });
+            });
+        },
+        open() {
+            const h = this.$createElement;
+
+            this.$notify({
+                title: 'Title',
+                message: h('p', { style: 'color: red' }, 'This is a reminder')
+            });
+        },
+    }
+})
---
+++ html-resources/js/core/settingsWindow.js
@@ -0,0 +1,19 @@
+﻿new Vue({
+    el: '#reactApp',
+    data: {
+        dialogVisible: false,
+        activeName: 'first',
+        reactiveVersion: commonCommands.reactiveVersion,
+        cefVersion: commonCommands.cefVersion + '| Revision: ' + commonCommands.cefHash,
+        newUsername: '',
+    },
+    methods: {
+        handleClick(tab, event) {
+            console.log(tab, event);
+        },
+        saveForm() {
+            userManager.userName = this.newUsername;
+            userManager.SaveSettings();
+        }
+    }
+})
